4123f88e00fedc0a10987734e5f341ff5ad0727a
x7-zzl <76738135+x7-zzl@users.noreply.github.com>
2026-06-25 17:56:40 +0800

    Delete backend directory
    删除分支

--- backend/src/main/java/com/zzl/umr/mapper/BasicArticleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zzl.umr.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zzl.umr.model.BasicArticle;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BasicArticleMapper extends BaseMapper<BasicArticle> {
-}
